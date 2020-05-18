a2bfa1a5857a25fd443d4bc998d199d9a0b482ef
AnatolyOrudgzhev <anatoly@pr-solution.ru>
2020-05-18 18:42:48 +0300

    added helpers LayoutMagic

---
+++ TestTaskH&H/TestTaskH&H/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTaskH&H
+//
+//  Created by Анатолий Оруджев on 18.05.2020.
+//  Copyright © 2020 Istvud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
